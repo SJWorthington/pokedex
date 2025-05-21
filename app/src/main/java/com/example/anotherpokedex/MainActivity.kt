813ac4811ba9182f332f85de8573fea067d246a5
Stephen Worthington <stephen.john.worthington@gmail.com>
2025-05-22 11:44:25 +1200

    Clean imports in Main Activity

--- app/src/main/java/com/example/anotherpokedex/MainActivity.kt
+++ app/src/main/java/com/example/anotherpokedex/MainActivity.kt
@@ -5,15 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.anotherpokedex.ui.screens.pokemonlist.PokemonList
 import com.example.anotherpokedex.ui.theme.AnotherPokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
